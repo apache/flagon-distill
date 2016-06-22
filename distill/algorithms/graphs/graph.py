9cf93fae230e01efda6a4ad83c13c2b5539a3873
mooshu1x2 <mbeard@draper.com>
2016-06-22 17:01:02 -0400

    Clean up algoriths/stats package for documentation purposes.

---
+++ distill/algorithms/graphs/graph.py
@@ -0,0 +1,9 @@
+class GraphAnalytics (object):
+	"""
+	Distill's graph analytics package. Apply graph algorithms to User Ale log data segmented with
+	Stout.
+	"""
+	
+	@staticmethod
+	def foo ():
+		pass
---
+++ distill/algorithms/stats/stats.py
@@ -0,0 +1,9 @@
+class Stats (object):
+	"""
+	Distill's statistics package. Apply statistical algorithms to User Ale log data segmented with
+	Stout.
+	"""
+	
+	@staticmethod
+	def foo ():
+		pass
