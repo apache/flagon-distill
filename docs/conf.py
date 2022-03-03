1e3e8d3fbd0009ba552408f0cb1d6ceddfd2b54d
Amir Ghaemi <amirmghaemi@yahoo.com>
2022-03-03 12:42:44 -0500

    Testing the main.yml

--- docs/conf.py
+++ docs/conf.py
@@ -331,7 +331,10 @@ author = 'Amir Ghaemi'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_dark_mode"
+
+# extensions = ["sphinx_rtd_dark_mode"
+# ]
+extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -348,7 +351,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
