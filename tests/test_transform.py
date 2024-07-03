25cc77dfe3ddb27eaf8962731db85d48754fa6e1
Broden222 <rodenbreonna@gmail.com>
2024-07-03 10:17:29 -0400

    feature_definition, tranform, and test_transform updates

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -14,7 +14,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 import distill
+from tests import testing_utils
+from tests.data_config import DATA_DIR
+import pytest
+from core import feature_definition
 
 
 def test_pairwiseStag_1():
@@ -39,3 +45,24 @@ def test_pairwiseSeq_2():
     test_list = [1, 2, 3, 4]
     result = distill.pairwiseSeq(test_list, split=True)
     assert result == ((1, 2, 3), (2, 3, 4))
+    
+data = testing_utils.setup(os.path.join(DATA_DIR, "sample_data.json"), "integer")
+logs = data[1]
+
+def input_rule(log):
+    return "target" in log and "input" in log["target"]
+
+def test_label_features_1():
+    result = distill.label_features(logs,[FeatureDefinition(input_rule, "input_target")])
+    assert result
+
+def test_label_features_2():
+    with pytest.raises(TypeError):
+        result = distill.label_features(logs,[FeatureDefinition(input_rule, 10)])
+
+def test_label_features_2():
+    with pytest.raises(TypeError):
+        result = distill.label_features(logs,[FeatureDefinition("input_rule", "input_target")])
+    
+
+
