c127bc9cb09d32bf67674349089dc797239cae65
Broden222 <154264811+Broden222@users.noreply.github.com>
2024-07-15 12:01:01 -0400

    38 add support to label logs (#44)
    * draft 38 add support to label logs
    
    * 38 add support label logs changes
    
    * 38 add support label logs changes
    
    * feature_definition, tranform, and test_transform updates
    
    * Should I create the process.py in the distill/process directory. Also, should the label function remain in the transform.py as well?
    
    * updated transform.py test_transform.py, and labels.py
    
    * test_transform.py
    
    * updated test_transform.py

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -14,7 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 import distill
+from tests import testing_utils
+from tests.data_config import DATA_DIR
+import pytest
+import json
+from distill.core.feature_definition import FeatureDefinition
+from typing import Any, Dict, List, Callable
+from distill.process.transform import label_features
+
 
 
 def test_pairwiseStag_1():
@@ -39,3 +49,31 @@ def test_pairwiseSeq_2():
     test_list = [1, 2, 3, 4]
     result = distill.pairwiseSeq(test_list, split=True)
     assert result == ((1, 2, 3), (2, 3, 4))
+
+def test_label_features():
+    file = open(os.path.join(DATA_DIR, "sample_data.json"), "r")
+    logs = json.load(file)
+    def type_rule(log) -> bool:
+        return "type" in log and "scroll" in log["type"]
+    result = label_features(logs,[FeatureDefinition(rule=type_rule, label="scroll_type")])
+    assert isinstance(result, list)
+    assert "labels" in set().union(*result)
+    assert 'labels', 'scroll_type' in result[1].items()
+
+
+def test_feature_definition_does_not_accept_non_string_label():
+    with pytest.raises(TypeError):
+        file = open(os.path.join(DATA_DIR, "sample_data.json"), "r")
+        logs = json.load(file)
+        def input_rule(log):
+            return "target" in log and "input" in log["target"]
+        result = label_features(logs,[FeatureDefinition(rule=input_rule, label=10)])
+
+def test_feature_definition_does_not_accept_non_callable_rules():
+    with pytest.raises(TypeError):
+        file = open(os.path.join(DATA_DIR, "sample_data.json"), "r")
+        logs = json.load(file)
+        result = label_features(logs,[FeatureDefinition(rule="input_rule", label="input_target")])
+    
+
+
